ebb0ac768eecfdc590866d370707ca6697b3f2d6
begin2ee <108245208+begin2ee@users.noreply.github.com>
2024-06-08 23:28:26 +0900

    [Silver IV] Title: 설탕 배달, Time: 124 ms, Memory: 14236 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int cnt = 0;
+
+        while(n>0){
+            if(n%5 == 0){
+                cnt += n/5;
+                break;
+            }
+
+            else{
+                n -= 3;
+                cnt++;
+            }
+
+            if(n<0)
+                cnt = -1;
+        }
+
+        System.out.println(cnt);
+       
+    }
+}
