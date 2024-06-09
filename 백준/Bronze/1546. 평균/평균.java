dce86d95f206049171bb05039aa1d95128898408
begin2ee <108245208+begin2ee@users.noreply.github.com>
2024-06-09 11:22:47 +0900

    [Bronze I] Title: 평균, Time: 132 ms, Memory: 14380 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -1,29 +1,27 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
-public class Main{
-    public static void main(String[] args){
-        Scanner s = new Scanner(System.in);
-        int n= s.nextInt();
-        double arr[]=new double[n];
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        double arr[] = new double[n];
 
-        double max=0;
-        for(int i=0; i<arr.length; i++){
-            arr[i]=s.nextDouble();
-            if(arr[i]>max){
-                max=arr[i];
-            }
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for(int i=0; i<n; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        double sum=0;
-        for(int i=0; i<arr.length; i++){
-            arr[i]=(arr[i]/max)*100.0;
-            
+        Arrays.sort(arr);
+        double max = arr[n-1];
 
-            sum +=arr[i];
+        double sum = 0;
+        for(int i=0; i<n; i++){
+            arr[i] = (arr[i]/max) * 100;
+            sum += arr[i];
         }
 
         System.out.println(sum/n);
-
     }
 }
